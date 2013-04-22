a3c817631f899bf3011403f111afd340658c0ef3
USB 16GB <thanhhai@usb-16gb>
2013-04-22 13:21:23 +0800

    added 201304_Apr22

--- paper_dev/clean.sh
+++ paper_dev/clean.sh
@@ -4,3 +4,9 @@ cp article.bib b.article.bib
 rm article.*
 mv b.article.tex article.tex
 mv b.article.bib article.bib 
+
+cp letter_to_reviewers.tex b.letter_to_reviewers.tex
+rm letter_to_reviewers.*
+mv b.letter_to_reviewers.tex letter_to_reviewers.tex
+
+
