30212127d64ba287690c7f4c530351a8f3086ddf
U-APP-SG-ISI\thanhhail <thanhhail@Thanh-PC.app-sg-isi.i-sprint.com>
2013-10-02 11:03:55 +0800

    ready to submit to ian & nick

---
+++ paper_dev/cygwin_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$(expr substr $(uname -s) 1 6)" == "CYGWIN" ]; then
+  #echo $PATH
+  export MIKTEX_USERSTARTUPFILE=D:\\workspaces\\portable\\MikTex\\miktex\\config\\miktexstartup.ini
+  echo $MIKTEX_USERSTARTUPFILE
+  #D:\workspaces\portable\MikTex\miktex\config\miktexstartup.ini
+  export MIKTEX_BINDIR=/cygdrive/d/workspaces/portable/MikTex/miktex/bin/
+  export PATH=$PATH:/cygdrive/d/workspaces/portable/MikTex/miktex/bin/
+  export TEMP=/cygdrive/d/workspaces/portable/MikTex/miktex/temp/
+  export TMP=/cygdrive/d/workspaces/portable/MikTex/miktex/temp/
+  export TEMPDIR=/cygdrive/d/workspaces/portable/MikTex/miktex/temp/
+  export TMPDIR=/cygdrive/d/workspaces/portable/MikTex/miktex/temp
+
+  latex 'article.tex'
+  bibtex article
+  latex 'article.tex'
+  latex 'article.tex'
+
+fi
+
