4ce260abe0b60209e1ceaa63a0e362e441874008
U-HAI-LAPTOP\user <user@Hai-Laptop.(none)>
2014-04-08 11:07:05 +0800

    auto

--- answer_thesis_examiner/cygwin_setup.sh
+++ answer_thesis_examiner/cygwin_setup.sh
@@ -2,19 +2,25 @@
 
 if [ "$(expr substr $(uname -s) 1 6)" == "CYGWIN" ]; then
   #echo $PATH
-  export MIKTEX_USERSTARTUPFILE=c:\\workspaces\\isprint_desktop\\portable\\MikTex\\miktex\\config\\miktexstartup.ini
+  export MIKTEX_USERSTARTUPFILE=c:\\workspaces\\portable\\MikTex\\miktex\\config\\miktexstartup.ini
   echo $MIKTEX_USERSTARTUPFILE
   #D:\workspaces\portable\MikTex\miktex\config\miktexstartup.ini
-  export MIKTEX_BINDIR=/cygdrive/c/workspaces/isprint_desktop/portable/MikTex/miktex/bin/
-  export PATH=$PATH:/cygdrive/c/workspaces/isprint_desktop/portable/MikTex/miktex/bin/
-  export TEMP=/cygdrive/c/workspaces/isprint_desktop/portable/MikTex/miktex/temp/
-  export TMP=/cygdrive/c/workspaces/isprint_desktop/portable/MikTex/miktex/temp/
-  export TEMPDIR=/cygdrive/c/workspaces/isprint_desktop/portable/MikTex/miktex/temp/
-  export TMPDIR=/cygdrive/c/workspaces/isprint_desktop/portable/MikTex/miktex/temp
+  export MIKTEX_BINDIR=/cygdrive/c/workspaces/portable/MikTex/miktex/bin/
+  export PATH=$PATH:/cygdrive/c/workspaces/portable/MikTex/miktex/bin/
+  export TEMP=/cygdrive/c/workspaces/portable/MikTex/miktex/temp/
+  export TMP=/cygdrive/c/workspaces/portable/MikTex/miktex/temp/
+  export TEMPDIR=/cygdrive/c/workspaces/portable/MikTex/miktex/temp/
+  export TMPDIR=/cygdrive/c/workspaces/portable/MikTex/miktex/temp
 
 fi
 
-  pdflatex 'answer_document.tex'
+  latex 'answer_document.tex'
+  for auxfile in chap*.aux
+  do
+      bibtex `basename $auxfile .aux`
+  done
+  latex 'answer_document.tex'
+  latex 'answer_document.tex'
   #bibtex thesis
   #latex 'thesis.tex'
   #latex 'thesis.tex'
